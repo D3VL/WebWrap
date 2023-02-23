cf074487ca24c1903e3a77720cb028f6616663fe
D3VL Jack <jack@d3vl.com>
2023-02-23 20:03:38 +0000

    WebWrap

--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"os"
-	"os/signal"
-	"embed"
-	"net"
-	"strconv"
+    "os"
+    "os/signal"
+    "embed"
+    "net"
+    "strconv"
 )
 
 // import internal packages
@@ -19,53 +19,53 @@ import (
 var embeddedFS embed.FS
 
 func findAvailablePort() (int, error) {
-	address, err := net.ResolveTCPAddr("tcp", "localhost:0")
-	if err != nil {
-		return 0, err
-	}
+    address, err := net.ResolveTCPAddr("tcp", "localhost:0")
+    if err != nil {
+        return 0, err
+    }
 
-	listener, err := net.ListenTCP("tcp", address)
-	if err != nil {
-		return 0, err
-	}
-	defer listener.Close()
-	return listener.Addr().(*net.TCPAddr).Port, nil
+    listener, err := net.ListenTCP("tcp", address)
+    if err != nil {
+        return 0, err
+    }
+    defer listener.Close()
+    return listener.Addr().(*net.TCPAddr).Port, nil
 }
 
 
 func main() {
-	// listen for interrupt signal
-	interrupt := make(chan os.Signal, 1)
-		signal.Notify(interrupt, os.Interrupt)
+    // listen for interrupt signal
+    interrupt := make(chan os.Signal, 1)
+        signal.Notify(interrupt, os.Interrupt)
 
-	 
+     
     log.EnableVerbose()
-	log.Debug("WebWrap Initializing...")
+    log.Debug("WebWrap Initializing...")
 
-	log.Info("Starting...")
+    log.Info("Starting...")
 
-	// generate a random port number
-	portInt, err := findAvailablePort()
-	if err != nil {
-		log.Error("Error finding an available port: " + err.Error())
-		return
-	}
+    // generate a random port number
+    portInt, err := findAvailablePort()
+    if err != nil {
+        log.Error("Error finding an available port: " + err.Error())
+        return
+    }
 
-	port := strconv.Itoa(portInt)
-	
-	go func() {
+    port := strconv.Itoa(portInt)
+    
+    go func() {
         server.Start(embeddedFS, port);
     }()
-	
-	browser.Open("http://127.0.0.1:" + port)
+    
+    browser.Open("http://127.0.0.1:" + port)
 
-	log.Info("When you are done, close this window to exit")
+    log.Info("When you are done, close this window to exit")
 
-	for {
-		select {
+    for {
+        select {
             case <-interrupt:
                 log.Debug("Process interrupted")
                 return
-		}
-	}
+        }
+    }
 }
--- packages/logging/logging.go
+++ packages/logging/logging.go
@@ -1,54 +1,54 @@
 package log
 
 import (
-	"fmt"
-	"time"
-	"os"
+    "fmt"
+    "time"
+    "os"
 )
 
 var logLevel = "INFO"
 
 func suppressLog(level string) bool {
-	if ( logLevel != "VERBOSE" && level != "INFO" && level != "ERROR" ) {
-		return true
-	} 
-	return false
+    if ( logLevel != "VERBOSE" && level != "INFO" && level != "ERROR" ) {
+        return true
+    } 
+    return false
 }
 
 // filter log level by 
 
 // export printLog function
 func PrintLog(data string, level string) {
-	if suppressLog(level) {
-		return
-	}
+    if suppressLog(level) {
+        return
+    }
 
-	time := time.Now().Format("2006-01-02 15:04:05")
-	fmt.Printf("[%s][%s] %s\n", level, time, data)
+    time := time.Now().Format("2006-01-02 15:04:05")
+    fmt.Printf("[%s][%s] %s\n", level, time, data)
 }
 
 func Error(data string) {
-	PrintLog(data, "ERROR")
+    PrintLog(data, "ERROR")
 }
 
 func Warning(data string) {
-	PrintLog(data, "WARNING")
+    PrintLog(data, "WARNING")
 }
 
 func Info(data string) {
-	PrintLog(data, "INFO")
+    PrintLog(data, "INFO")
 }
 
 func Debug(data string) {
-	PrintLog(data, "DEBUG")
+    PrintLog(data, "DEBUG")
 }
 
 func Fatal(err error) {
-	// print then exit
-	PrintLog(err.Error(), "FATAL")
-	os.Exit(1)
+    // print then exit
+    PrintLog(err.Error(), "FATAL")
+    os.Exit(1)
 }
 
 func EnableVerbose() {
-	logLevel = "VERBOSE"
+    logLevel = "VERBOSE"
 }
